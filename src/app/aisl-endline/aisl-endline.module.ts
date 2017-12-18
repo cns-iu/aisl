f9bda1f6cd2aed1675e09f99828b0cd8347d6018
Bolin <axbolin@ads.iu.edu>
2017-12-18 13:30:02 -0500

    Created endline module + time-display comp.

---
+++ src/app/aisl-endline/aisl-endline.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TimeDisplayComponent } from './time-display/time-display.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TimeDisplayComponent]
+})
+export class AislEndlineModule { }
---
+++ src/app/aisl-endline/time-display/time-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'aisl-time-display',
+  templateUrl: './time-display.component.html',
+  styleUrls: ['./time-display.component.sass']
+})
+export class TimeDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
