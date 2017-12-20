a13b15a98ab53aa943319e22a7186dfedf88e80e
Daniel Bolin <axbolin@iu.edu>
2017-12-20 12:00:15 -0500

    Implemented lanes

--- src/app/aisl-endline/lane/lane.component.ts
+++ src/app/aisl-endline/lane/lane.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Duration, duration } from 'moment';
+
+import { MessageService } from '../../aisl-backend';
+import { RunSelectedMessage, RaceCompletedMessage, RaceResult } from '../../aisl-backend';
 
 @Component({
   selector: 'aisl-lane',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lane.component.sass']
 })
 export class LaneComponent implements OnInit {
+  @Input() laneId: number;
+  @Input() timeFormat: string | null = null;
 
-  constructor() { }
+  private falseStart: boolean = false;
+  private time?: Duration;
+
+  constructor(private messageService: MessageService) {
+    this.messageService.asObservable().subscribe((msg) => {
+      if (msg instanceof RunSelectedMessage) {
+        this.resetResult();
+      } else if (msg instanceof RaceCompletedMessage) {
+        const result = msg.results.find((result) => {
+          return result.lane == this.laneId;
+        });
+
+        if (result !== undefined) {
+          this.setResult(result);
+        }
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  get available(): boolean {
+    return this.time !== undefined;
+  }
+
+  private setResult(result: RaceResult): void {
+    this.time = duration(result.timeMillis);
+    this.falseStart = result.falseStart;
+  }
+
+  private resetResult(): void {
+    this.time = undefined;
+    this.falseStart = false;
   }
 
 }
