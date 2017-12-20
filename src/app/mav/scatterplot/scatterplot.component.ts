9d8605f43555dd5ba22c63049277256f10a1dae9
Gagandeep Singh <whg_27@yahoo.com>
2017-12-20 00:58:52 -0500

    basic scatterplot
    A basic d3 v4 scatterplot with drop-target directive set. Fields can be dropped on the scatterplot and detected by the directive at this point in code. It logs the dropped field-names in the console.

--- src/app/mav/scatterplot/scatterplot.component.ts
+++ src/app/mav/scatterplot/scatterplot.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -26,15 +26,66 @@ import * as d3 from 'd3';
 })
 export class ScatterplotComponent implements OnInit {
 
-  constructor() { }
+  private parentNativeElement: any; // a native Element to access this component's selector for drawing the map
+  svgContainer=null;
+  constructor(element: ElementRef) {
+    this.parentNativeElement = element.nativeElement; //to get native parent element of this component
 
- drawScatterplot(){// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-   // set the ranges
-   var x = d3.scaleLinear().range([0, width]);
-   var y = d3.scaleLinear().range([height, 0]);
+  }
+
+  drawScatterplot(){
+    var data = [[5,3], [10,17], [15,4], [2,8]];
+
+    var margin = {top: 20, right: 15, bottom: 60, left: 60}
+    , width = 960 - margin.left - margin.right
+    , height = 500 - margin.top - margin.bottom;
+
+    var x =  d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d[0]; })])
+    .range([ 0, width ]);
+
+    var y =  d3.scaleLinear()
+    .domain([0, d3.max(data, function(d) { return d[1]; })])
+    .range([ height, 0 ]);
+    //initializing svg container
+    this.svgContainer = d3.select(this.parentNativeElement).select("#plotContainer")
+    .append("svg")
+    .attr('width', width + margin.right + margin.left)
+    .attr('height', height + margin.top + margin.bottom)
+    .attr('class', 'chart')
+    var main = this.svgContainer.append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('class', 'main')
+
+    // draw the x axis
+    var xAxis = d3.axisBottom(x);
+
+    main.append('g')
+    .attr('transform', 'translate(0,' + height + ')')
+    .attr('class', 'main axis date')
+    .call(xAxis);
+
+    // draw the y axis
+    var yAxis = d3.axisLeft(y);
+
+    main.append('g')
+    .attr('transform', 'translate(0,0)')
+    .attr('class', 'main axis date')
+    .call(yAxis);
+
+    var g = main.append("svg:g");
+
+    g.selectAll("scatter-dots")
+    .data(data)
+    .enter().append("svg:circle")
+    .attr("cx", function (d,i) { return x(d[0]); } )
+    .attr("cy", function (d) { return y(d[1]); } )
+    .attr("r", 8);
+
+
+
   }
 
   ngOnInit() {
