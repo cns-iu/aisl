fe3a168b649089820c5c0731d7144ceb16e522b7
Daniel Bolin <axbolin@iu.edu>
2018-01-16 15:28:34 -0500

    Added typing for json files

--- src/app/dino-geomap/geomap/geomap.component.ts
+++ src/app/dino-geomap/geomap/geomap.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { vega, defaultLogLevel } from '../../vega';
+import * as us10m from './us-10m.json';
+
 @Component({
   selector: 'dino-geomap',
   templateUrl: './geomap.component.html',
